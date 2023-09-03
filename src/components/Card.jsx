bf6744a709dd60956399eaa5a39d06f12926981d
CMLTTN <cemaldim@gmail.com>
2023-09-03 16:04:33 +0300

    card

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import Image from "./styled/Image";
+import StyledCard from "./styled/CardStyles";
+
+const Card = ({ id, title, body, image }) => {
+  return (
+    <StyledCard odd={id % 2 && "row-reverse"} >
+
+    <div>
+     
+    </div>
+      <h2>{title}</h2>
+      <p>{body}</p>
+      <div>
+        <Image src={`./images/${image}`} />
+      </div>
+    </StyledCard>
+  );
+};
+
+export default Card;
